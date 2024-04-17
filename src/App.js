c422562732b6080b246ba74478abaf9e682ab571
utkarsh <vyasutkarsh11@gmail.com>
2024-04-17 17:39:50 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Lock from "./artifacts/contracts/Lock.sol/Lock.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Lock.abi,
+          signer
+        );
+        console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+  }, []);
+  return <div className="App">
+
+    <>
+    {account}
+    </>
+  </div>;
 }
 
 export default App;
